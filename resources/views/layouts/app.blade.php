a501234baae15a72798e51ce9c8c9da9f85a2fb6
KSouzaEng <krmcristine@gmail.com>
2023-01-07 15:26:40 -0400

    add broadcasting event

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+@vite('resources/js/app.js')
 <link
 href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
 rel="stylesheet"
--- routes/channels.php
+++ routes/channels.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Broadcast;
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
+
+Broadcast::channel('order', function () {
+    return true;
+});
+Broadcast::channel('channel-publico', function(){
+    return true;
+});
